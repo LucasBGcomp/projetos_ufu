f167d3030a05495a48a0e4a1cd21da9d4cdf98ff
LucasBGcomp <lucasbgcomp@gmail.com>
2025-12-04 16:32:17 -0300

    ex5

---
+++ PP/Trabalho1/Ex5.c
@@ -0,0 +1,19 @@
+/*Faça um programa para verificar se um determinado ano lido é bissexto, imprimindo “SIM” em caso afirmativo ou “NAO”, caso contrário. Um ano é bissexto se for divisível por 400, ou se for divisível por 4 e não for divisível por 100.*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    int ano;
+
+    scanf("%d", &ano);
+
+    if ((ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0)) {
+        printf("SIM\n");
+    } else {
+        printf("NAO\n");
+    }
+
+    return 0;
+
+}
