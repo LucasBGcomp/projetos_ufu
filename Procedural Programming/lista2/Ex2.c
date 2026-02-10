db066f763783d2aab897a4fb6ea827dc908ff827
LucasBGcomp <lucasbgcomp@gmail.com>
2026-02-10 17:10:48 -0300

    Ex2

---
+++ Procedural Programming/lista2/Ex2.c
@@ -0,0 +1,42 @@
+/*Abra e leia o arquivo criado no exercício anterior, caractere por caractere, escreva na tela todos os
+caracteres e mostre na tela quantas letras são vogais e quantas são consoantes.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+int main() {
+
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    FILE *fp;
+    fp = fopen("C:\\lucas\\comp\\projetos_ufu\\Procedural Programming\\lista2\\output\\Exc_arquivos\\Ex1_arq.txt", "r");
+    if (fp == NULL) {
+        printf("Erro ao abrir o arquivo!");
+        exit (1);
+    }
+
+    char c;
+    int vogais = 0, consoantes = 0;
+    printf("Conteúdo do arquivo: ");
+    while ((c = fgetc(fp)) != EOF) {
+        if ((c >= 'a' && c<='z') || (c >= 'A' && c <= 'Z'))
+        {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                vogais++;
+            } else {
+                consoantes++;
+            }
+        }
+        printf("%c", c);
+    }
+
+    printf("\nQuantidade de vogais: %d\n", vogais);
+    printf("\nQuantidade de consoantes: %d\n\n", consoantes);
+
+    fclose(fp);
+    system("pause");
+    return 0;
+}
