fa3bca4e54aae4b847ddb1b29b00a63b0ca31ceb
LucasBGcomp <lucasbgcomp@gmail.com>
2026-02-10 17:39:51 -0300

    Ex4

---
+++ Procedural Programming/lista2/Ex4.c
@@ -0,0 +1,27 @@
+/*Escreva um programa C para gerar em um arquivo as tabuadas de 1 a 10.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    FILE *fp;
+    fp = fopen("C:\\lucas\\comp\\projetos_ufu\\Procedural Programming\\lista2\\output\\Exc_arquivos\\Ex4_arq.txt", "w");
+    if (fp == NULL){
+        printf("Erro ao abrir o arquivo!");
+        exit(1);
+    }
+
+    for (int i = 1; i <= 10; i++){
+        for (int j = 1; j <= 10; j++){
+            fprintf(fp, "%d x %d = %d\n", i, j, i * j);
+        }
+        fprintf(fp, "\n");
+    }
+
+    printf("Arquivo com tabuada de 1 a 10 criado com sucesso!\n\n");
+
+    fclose(fp);
+    system("pause");
+    return 0;
+}
