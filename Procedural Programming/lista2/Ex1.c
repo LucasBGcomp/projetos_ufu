636793e1ca0d95b5ee3da1486e3b7006a683ad08
LucasBGcomp <lucasbgcomp@gmail.com>
2026-02-10 16:32:55 -0300

    Ex1

---
+++ Procedural Programming/lista2/Ex1.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    FILE *fp;
+    fp = fopen("Ex1_arq.txt", "w");
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o arquivo!!\n");
+        exit(1);
+    }
+
+    char s[100];
+    printf("Digite uma string: ");
+    scanf("%99[^\n]s", s);
+    setbuf(stdin, NULL);
+    printf("\nString gravada no arquivo com sucesso!!\n\n");
+
+    fprintf(fp, "%s\n", s);
+
+    fclose(fp);
+    system("pause");
+    return 0;
+}
