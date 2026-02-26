233af7df9bbfa23ca3341c96fdc40f2f85d1b08f
LucasBGcomp <lucasbgcomp@gmail.com>
2026-02-26 15:46:41 -0300

    Ex24 e executaveis lista 2

---
+++ Procedural Programming/lista2/Ex24.c
@@ -0,0 +1,33 @@
+/*Escreva uma função que calcule a Sequência de Fibonacci, F, para um parâmetro n fornecido pelo
+usuário.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int fib(int n){
+
+    if (n == 0 || n ==1){
+        return n;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
+
+}
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese_Brazil.1252");
+
+    int n;
+    do{
+        printf("Digite um número inteiro maior que zero: ");
+        scanf("%d", &n);
+        setbuf(stdin, NULL);
+    } while (n <= 0);
+
+    printf("\nO %dº termo da sequência de Fibonacci é: %d\n\n", n, fib(n));
+    
+    system("pause");
+    return 0;
+}
