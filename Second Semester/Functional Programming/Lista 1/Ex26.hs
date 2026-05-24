e9c8cc135ebf828a7aa9f86baedbe964495aa622
Lucas Espíndula Borges <lucasbgcomp@gmail.com>
2026-05-24 20:03:54 -0300

    mod Ex26 L1 fp

--- Second Semester/Functional Programming/Lista 1/Ex26.hs
+++ Second Semester/Functional Programming/Lista 1/Ex26.hs
@@ -1,7 +1,7 @@
 altura :: Double -> Double -> Double
 altura v t
     | t == 0 = 0
-    | t < 0 = error "O tempo não pode ser negativo."
-    | v < 0 = error "A velocidade inicial não pode ser negativa."
+    | t < 0 = error "O tempo nao pode ser negativo."
+    | v < 0 = error "A velocidade inicial nao pode ser negativa."
     | otherwise = if h < 0 then 0 else h
     where h = (v * t) - (0.5 * 9.8 * t*t)
