728cfce02ca8d09d36a8b2ac3767e8bf57410442
Lucas Espíndula Borges <lucasbgcomp@gmail.com>
2026-05-24 19:16:35 -0300

    mod Ex2 L1 fp

--- Second Semester/Functional Programming/Lista 1/Ex2.hs
+++ Second Semester/Functional Programming/Lista 1/Ex2.hs
@@ -1,5 +1,6 @@
-import Data.Char(toUpper)
-
 maiusculas :: String -> String
 maiusculas [] = []
-maiusculas (x:xs) = toUpper x : maiusculas xs
+maiusculas (x:xs)
+    | x >= 'A' && x <= 'Z' = x : maiusculas xs
+    | x >= 'a' && x <= 'z' = (toEnum (fromEnum x - 32)) : maiusculas xs
+    | otherwise = x : maiusculas xs
