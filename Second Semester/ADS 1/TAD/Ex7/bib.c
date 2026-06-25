3038af8bed904848fc65423e4e24ebb38125c10f
Lucas Espíndula Borges <lucasbgcomp@gmail.com>
2026-06-24 22:14:39 -0300

    Ex7 TAD ads1 final

--- Second Semester/ADS 1/TAD/Ex7/bib.c
+++ Second Semester/ADS 1/TAD/Ex7/bib.c
@@ -1,46 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "bib.h"
 
-struct no {
+struct no
+{
     char nome[20];
     struct no *prox;
     struct no *ant;
 };
 
-struct descritor {
+struct descritor
+{
     No *inicio;
     int quantidade;
 };
 
-void inicializarLista (Descritor **d) {
+void inicializarLista(Descritor **d)
+{
     *d = (Descritor *)malloc(sizeof(Descritor));
     if (*d == NULL)
         return;
     (*d)->inicio = NULL;
-    (*d)->qtd_naves = 0;
+    (*d)->quantidade = 0;
 }
 
-int criarNo (No **novo)
+int criarNo(No **novo)
 {
-    *novo = (Nave *)malloc(sizeof(Nave));
+    *novo = (No *)malloc(sizeof(No));
     if (*novo == NULL)
         return 0; // Falha na alocação
     return 1;     // Sucesso
 }
 
-int inserirInicio (Descritor *d, char *nome) {
+int inserirInicio(Descritor *d, char *nome)
+{
     No *novo;
     if (!criarNo(&novo))
-        return 0; //falha
-    
-    strcpy(novo->nome,nome);
+        return 0; // falha
 
-    if (d->quantidade == 0) {
+    strcpy(novo->nome, nome);
+
+    if (d->quantidade == 0)
+    {
         d->inicio = novo;
         novo->prox = novo;
         novo->ant = novo;
-    } else {
+    }
+    else
+    {
         novo->prox = d->inicio;
         novo->ant = d->inicio->ant;
         novo->ant->prox = novo;
@@ -48,86 +56,114 @@ int inserirInicio (Descritor *d, char *nome) {
         d->inicio = novo;
     }
     d->quantidade++;
-    return 1; //sucesso
+    return 1; // sucesso
 }
 
-void moverParaFrente (Descritor *d, int k, No **atual) { 
-    *atual = d->inicio;
+void moverParaFrente(Descritor *d, int k, No **atual)
+{
+    if (d->quantidade == 0)
+        return;
     int p = 0;
 
-    while (p < k) {
+    while (p < (k % d->quantidade))
+    {
         *atual = (*atual)->prox;
         p++;
     }
 }
 
-void moverParaTras (Descritor *d, int k, No **atual) {
-    *atual = d->inicio;
+void moverParaTras(Descritor *d, int k, No **atual)
+{
+    if (d->quantidade == 0)
+        return;
     int p = 0;
 
-    while (p < k) {
+    while (p < (k % d->quantidade))
+    {
         *atual = (*atual)->ant;
         p++;
     }
 }
 
-int removerAtual (Descritor *d, No **atual) {
+int removerAtual(Descritor *d, No **atual)
+{
     if (d->quantidade == 0)
-        return 0; //falha
+        return 0; // falha
 
-    if ((*atual) == d->inicio) {
-        d->inicio = (*atual)->prox;
-        (*atual)->ant->prox = (*atual)->prox;
-        (*atual)->prox->ant = (*atual)->ant;
-    } else {
+    if (d->quantidade == 1)
+    {
+        d->inicio = NULL;
+    }
+    else
+    {
+        if (*atual == d->inicio)
+        {
+            d->inicio = (*atual)->prox;
+        }
         (*atual)->ant->prox = (*atual)->prox;
         (*atual)->prox->ant = (*atual)->ant;
     }
-    
-    free((*atual));
+
+    free(*atual);
+    *atual = NULL;
     d->quantidade--;
-    return 1; //sucesso
+    return 1; // sucesso
 }
 
-void lider (Descritor *d, int k) {
+void lider(Descritor *d, int k)
+{
     No *atual = d->inicio;
-    int op = 0;
+    int c = 0;
+    if (d->quantidade == 0)
+    {
+        printf("\nA lista está vazia!\n");
+        return;
+    }
 
-    while (d->quantidade > 1) {
-        if (op == 0) {
-            moverParaFrente (d, k, &atual);
+    while (d->quantidade > 1)
+    {
+        if (c == 0)
+        {
+            moverParaFrente(d, k, &atual);
             No *temp = atual;
-            atual = atual->prox;
-            removerAtual (d, &temp);
-            op = 1;
+            No *proxNo = atual->prox;
+            removerAtual(d, &temp);
+            atual = proxNo;
+            c = 1;
             continue;
         }
 
-        if (op == 1) {
-            moverParaTras (d, k, &atual);
+        if (c == 1)
+        {
+            moverParaTras(d, k, &atual);
             No *temp = atual;
-            atual = atual->ant;
-            removerAtual (d, &temp);
-            op = 0;
+            No *antNo = atual->ant;
+            removerAtual(d, &temp);
+            atual = antNo;
+            c = 0;
             continue;
         }
     }
+    printf("\nO líder sorteado é: %s\n", d->inicio->nome);
 }
 
-void limparBuffer () {
+void limparBuffer()
+{
     int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
 }
 
-void liberarLista (Descritor *d) {
+void liberarLista(Descritor *d)
+{
     if (d == NULL)
         return;
 
-    if (d->qtd_naves > 0)
+    if (d->quantidade > 0)
     {
-        Nave *atual = d->inicio;
-        Nave *temp;
-        for (int i = 0; i < d->qtd_naves; i++)
+        No *atual = d->inicio;
+        No *temp;
+        for (int i = 0; i < d->quantidade; i++)
         {
             temp = atual;
             atual = atual->prox;
--- Second Semester/ADS 1/TAD/Ex7/main.c
+++ Second Semester/ADS 1/TAD/Ex7/main.c
@@ -1,1 +1,58 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include "bib.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    int op;
+    srand(time(NULL));
+    Descritor *lista;
+    inicializarLista(&lista);
+    if (lista == NULL)
+    {
+        return 1;
+    }
+
+    do
+    {
+        printf("\n=======MENU=======\n\n1. Inserir Pessoa\n2. Sortear Líder\n0. Sair\n\nEscolha uma opção: ");
+        scanf("%d", &op);
+
+        switch (op)
+        {
+        case 1:
+        {
+            char nome[20];
+            limparBuffer();
+            printf("\nDigite o nome da pessoa: ");
+            scanf(" %19[^\n]", nome);
+
+            if (inserirInicio(lista, nome))
+            {
+                printf("\nInserção feita!\n");
+            }
+            else
+            {
+                printf("\nErro na inserção\n");
+            }
+            break;
+        }
+        case 2:
+            lider(lista, rand());
+            break;
+        case 0:
+            break;
+        default:
+            printf("\nOpção inválida! Tente novamente.\n");
+        }
+    } while (op != 0);
+
+    liberarLista(lista);
+    printf("\nPressione Enter para sair...");
+    getchar();
+    return 0;
+}
